a503de788b1792813b2bc40f9ecbb7bc966dcf27
Tran Doan Cuong <cu_tran@vision-net.co.jp>
2021-05-18 14:38:10 +0700

    first commit

---
+++ resources/views/chart.blade.php
@@ -0,0 +1,113 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700;800&display=swap" rel="stylesheet">
+    <link rel="icon" href="http://www.domain.com/favicon.ico" type="image/x-icon" />
+    <link rel="shortcut icon" href="http://www.domain.com/favicon.ico" type="image/x-icon" />
+    <title>SEO Dream</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" href="assets/css/fontawesome.css">
+    <link rel="stylesheet" href="assets/css/templatemo-seo-dream.css">
+    <link rel="stylesheet" href="assets/css/animated.css">
+    <link rel="stylesheet" href="assets/css/owl.css">
+    <!--
+
+    TemplateMo 563 SEO Dream
+
+    https://templatemo.com/tm-563-seo-dream
+
+    -->
+
+</head>
+
+<body>
+
+<!-- ***** Preloader Start ***** -->
+<div id="js-preloader" class="js-preloader">
+    <div class="preloader-inner">
+        <span class="dot"></span>
+        <div class="dots">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+    </div>
+</div>
+<!-- ***** Preloader End ***** -->
+
+<!-- ***** Header Area Start ***** -->
+<header class="header-area header-sticky wow slideInDown" data-wow-duration="0.75s" data-wow-delay="0s">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <nav class="main-nav">
+                    <!-- ***** Logo Start ***** -->
+                    <a href="/" class="logo">
+                        <h4>SEO Dream <img src="assets/images/logo-icon.png" alt=""></h4>
+                    </a>
+                    <!-- ***** Logo End ***** -->
+                    <!-- ***** Menu Start ***** -->
+                    <ul class="nav">
+                        <li class="scroll-to-section"><a href="/" class="active">Home</a></li>
+                        <li class="scroll-to-section"><a href="#features">Roadmap</a></li>
+                        <li class="scroll-to-section"><a href="#about">About Us</a></li>
+                        <li class="scroll-to-section"><a href="#services">Services</a></li>
+                        <li class="scroll-to-section"><a href="#portfolio">NFT Marketpalce</a></li>
+                        <li class="scroll-to-section"><a href="/chart">Chart</a></li>
+                        <li class="scroll-to-section"><div class="main-blue-button"><a href="#contact">BUY TOKEN (Coming soon)</a></div></li>
+                    </ul>
+                    <a class='menu-trigger'>
+                        <span>Menu</span>
+                    </a>
+                    <!-- ***** Menu End ***** -->
+                </nav>
+            </div>
+        </div>
+    </div>
+</header>
+<!-- ***** Header Area End ***** -->
+<div class="container-chart">
+    <iframe width="100%" id="tradingview_73e3c" name="tradingview_73e3c" src="https://dex.guru/token/0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c-bsc" frameborder="0" allowtransparency="true" scrolling="yes" allowfullscreen="" style="display: block; width: 100%; height: 1100px"></iframe>
+</div>
+
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-7">
+                <p>Copyright © 2021 SEO Dream Co., Ltd. All Rights Reserved.</p>
+            </div>
+            <div class="col-lg-5 col-md-12 col-sm-12">
+                <ul class="social">
+                    <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                    <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                    <li><a href="#"><i class="fa fa-rss"></i></a></li>
+                    <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<!-- Scripts -->
+<script src="vendor/jquery/jquery.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="assets/js/owl-carousel.js"></script>
+<script src="assets/js/animation.js"></script>
+<script src="assets/js/imagesloaded.js"></script>
+<script src="assets/js/custom.js"></script>
+
+</body>
+</html>
